3cb911f9dbb0a3887c474dae69b06a210e60f2e7
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-09-27 01:28:49 +0530

    Update password_decoder.v

--- security_alarm_system.srcs/sources_1/new/password_decoder.v
+++ security_alarm_system.srcs/sources_1/new/password_decoder.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.05.2024 12:56:42
-// Design Name: 
-// Module Name: password_decoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module password_decoder(
     i_password,
