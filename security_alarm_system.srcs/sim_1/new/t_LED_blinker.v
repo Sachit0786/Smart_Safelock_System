aaf8df386a6b0a7e76bf16fb0fe1f1ca03750522
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-09-27 01:33:47 +0530

    Update t_LED_blinker.v

--- security_alarm_system.srcs/sim_1/new/t_LED_blinker.v
+++ security_alarm_system.srcs/sim_1/new/t_LED_blinker.v
@@ -1,25 +1,5 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.05.2024 19:58:21
-// Design Name: 
-// Module Name: t_LED_blinker
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 `timescale 1ps/1ps
+
 module t_LED_blinker();
 
     reg clk;
