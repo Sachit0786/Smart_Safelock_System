507aef70cacb24e442b24aee67d279e2d213440c
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-09-27 01:33:12 +0530

    Update t_password_decoder.v

--- security_alarm_system.srcs/sim_1/new/t_password_decoder.v
+++ security_alarm_system.srcs/sim_1/new/t_password_decoder.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.05.2024 20:07:51
-// Design Name: 
-// Module Name: t_password_decoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module t_password_decoder();
 
